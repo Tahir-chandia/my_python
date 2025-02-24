ecc57771a6b126dc1cc3dd7c6a93cdcbbdf764ef
Tahir-Chandia <mt3523559@gmail.com>
2025-02-24 17:07:50 +0500

    first

---
+++ challenge series/Day01/day1.py
@@ -0,0 +1,8 @@
+# Day 1- Python Challange:
+# Print First Message Like This: "Hello, Python Learners! Today is the first step in our coding journey."
+
+print('"Hello, Python Learners! Today is the first step in our coding journey."')  
+
+# Modifying this by adding name
+my_name:str ="Tahir"
+print(f'"Hello, {my_name}! Today is the first step in our coding journey."')#Here is use f string to print my name
